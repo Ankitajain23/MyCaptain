95f9c26beb5678a6bf3f75aca6cbfbca00f810f1
Ankitajain23 <66482105+Ankitajain23@users.noreply.github.com>
2020-06-06 00:44:10 +0530

    Create product.cpp

---
+++ product.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+
+int main()
+{
+    
+  double a,b,c;
+    cout<<"Enter the two numbers:";
+    cin>>a;
+    cin>>b;
+    c=a*b;
+    cout<<"Product="<<c;
+    return 0;
+}
